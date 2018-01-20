57208161071bac8b6314b82466b9723f3c5287fa
Onno <mabusraeen@gmail.com>
2018-01-20 11:18:12 +0100

    Figured out what I want to do with a datagrid.

---
+++ DataGridWithObjectData/DataGridWithObjectData.Designer.cs
@@ -0,0 +1,103 @@
+﻿namespace DataGridWithObjectData
+{
+    partial class DataGridWithObjectData
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.DataGrid = new System.Windows.Forms.DataGridView();
+            this.Add = new System.Windows.Forms.Button();
+            this.Remove = new System.Windows.Forms.Button();
+            this.Save = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // DataGrid
+            // 
+            this.DataGrid.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.DataGrid.Location = new System.Drawing.Point(0, 41);
+            this.DataGrid.Name = "DataGrid";
+            this.DataGrid.Size = new System.Drawing.Size(773, 434);
+            this.DataGrid.TabIndex = 0;
+            this.DataGrid.AllowUserToAddRows = false;
+
+            // 
+            // Add
+            // 
+            this.Add.Location = new System.Drawing.Point(0, 12);
+            this.Add.Name = "Add";
+            this.Add.Size = new System.Drawing.Size(75, 23);
+            this.Add.TabIndex = 1;
+            this.Add.Text = "Add";
+            this.Add.UseVisualStyleBackColor = true;
+            this.Add.Click += new System.EventHandler(this.Add_Click);
+            // 
+            // Remove
+            // 
+            this.Remove.Location = new System.Drawing.Point(81, 12);
+            this.Remove.Name = "Remove";
+            this.Remove.Size = new System.Drawing.Size(75, 23);
+            this.Remove.TabIndex = 2;
+            this.Remove.Text = "Remove";
+            this.Remove.UseVisualStyleBackColor = true;
+            this.Remove.Click += new System.EventHandler(this.Remove_Click);
+            // 
+            // Save
+            // 
+            this.Save.Location = new System.Drawing.Point(162, 12);
+            this.Save.Name = "Save";
+            this.Save.Size = new System.Drawing.Size(75, 23);
+            this.Save.TabIndex = 3;
+            this.Save.Text = "Save";
+            this.Save.UseVisualStyleBackColor = true;
+            this.Save.Click += new System.EventHandler(this.Save_Click);
+            // 
+            // DataGridWithObjectData
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(773, 475);
+            this.Controls.Add(this.Save);
+            this.Controls.Add(this.Remove);
+            this.Controls.Add(this.Add);
+            this.Controls.Add(this.DataGrid);
+            this.Name = "DataGridWithObjectData";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.DataGridWithObjectData_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.DataGrid)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView DataGrid;
+        private System.Windows.Forms.Button Add;
+        private System.Windows.Forms.Button Remove;
+        private System.Windows.Forms.Button Save;
+    }
+}
+
---
+++ DataGridWithObjectData/DataGridWithObjectData.cs
@@ -0,0 +1,68 @@
+﻿/* An experimental program that uses a BindList as a Datasource linked to a DataGrid.
+ * I'm storing a serialized string an in application settings to see if it is possible to serialize and deserialize data.
+ * Guess what... it worked. 
+ * OI
+ */
+
+using DataGridWithObjectData.Properties;
+using System;
+using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace DataGridWithObjectData
+{
+    public partial class DataGridWithObjectData : Form
+    {
+
+        private BindingSource bindingSource = new BindingSource();
+        private BindingList<Data> lijst = new BindingList<Data>();
+
+        public DataGridWithObjectData()
+        {
+            InitializeComponent();
+        }
+
+        private void DataGridWithObjectData_Load(object sender, EventArgs e)
+        {
+            if (Settings.Default.Data == string.Empty)
+            {
+                // Add some test data
+                lijst.Add(new Data { Name = "Piet", Age = 39 });
+                lijst.Add(new Data { Name = "Klaas", Age = 35 });
+                lijst.Add(new Data { Name = "Joost", Age = 12 });
+            }
+            else
+            {
+                lijst = Newtonsoft.Json.JsonConvert.DeserializeObject<BindingList<Data>>(Settings.Default.Data);
+            }
+
+            bindingSource.DataSource = lijst;
+
+            DataGrid.AutoGenerateColumns = true;
+            DataGrid.AutoSize = true;
+            DataGrid.DataSource = bindingSource;
+        }
+
+        private void Add_Click(object sender, EventArgs e)
+        {
+            this.lijst.Add(new Data());
+        }
+
+        private void Remove_Click(object sender, EventArgs e)
+        {
+            var listItem = DataGrid.CurrentRow.DataBoundItem as Data;
+            this.lijst.Remove(listItem);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Save_Click(object sender, EventArgs e)
+        {
+            Settings.Default.Data = Newtonsoft.Json.JsonConvert.SerializeObject(lijst);
+            Settings.Default.Save();
+        }
+    }
+}
