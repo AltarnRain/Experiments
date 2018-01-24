42a2da4dd89cfc964acf1ce8c6cab763624a9983
Onno <mabusraeen@gmail.com>
2018-01-24 15:09:06 +0100

    Figured out databinding for textbox and checkbox.

---
+++ DataBindTextBox/MainForm.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataBindTextBox
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public Model model = new Model();
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            this.textBox1.DataBindings.Add("Text",
+                                this.model,
+                                "Name",
+                                false,
+                                DataSourceUpdateMode.OnPropertyChanged);
+
+            this.checkBox1.DataBindings.Add("Checked",
+                             this.model,
+                             "Checked",
+                             false,
+                             DataSourceUpdateMode.OnPropertyChanged);
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(this.model.Name);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.model.Name = "Pet";
+            this.model.Checked = true;
+        }
+    }
+
+
+}
---
+++ DataBindTextBox/Model.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataBindTextBox
+{
+    public class Model : INotifyPropertyChanged
+    {
+        private string name;
+        private bool check;
+
+        public string Name
+        {
+            get
+            {
+                return this.name;
+            }
+            set
+            {
+                if (this.name != value)
+                {
+                    this.name = value;
+                    this.NotifyPropertyChanged();
+                }
+
+            }
+        }
+
+        public bool Checked
+        {
+            get => check;
+            set
+            {
+                if (check != value)
+                {
+                    this.check = value;
+                    this.NotifyPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        private void NotifyPropertyChanged([CallerMemberName] String propertyName = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+    }
+}
